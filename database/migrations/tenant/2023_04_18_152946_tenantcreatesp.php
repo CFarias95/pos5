836d469e32b8665df2b748951db7b6a3331a5f69
desarrollo@joinec.net <desarrollo@joinec.net>
2024-01-02 17:54:25 -0500

    ajuste

--- database/migrations/tenant/2023_04_18_152946_tenantcreatesp.php
+++ database/migrations/tenant/2023_04_18_152946_tenantcreatesp.php
@@ -14,20 +14,18 @@ class Tenantcreatesp extends Migration
     {
         //Para crear, altera, inserta
         //Nombre Cliente total Facturado
-        $sql = "
-        
 
-        CREATE PROCEDURE SP_VentasPorCliente(IN cliente_nombre VARCHAR(35))
+        $sqlDelete = "DROP PROCEDURE IF EXISTS SP_VentasPorCliente";
+        $sql = "CREATE PROCEDURE `SP_VentasPorCliente`(IN cliente_nombre VARCHAR(35))
         BEGIN
             SELECT d.customer_id AS 'ID Cliente', p.name AS 'Nombre Cliente', SUM(d.total) AS 'Total Venta' FROM persons p
-            INNER JOIN documents d 
-            ON p.id = d.customer_id
+            INNER JOIN documents d ON p.id = d.customer_id
             WHERE p.name = cliente_nombre
             GROUP BY d.customer_id;
         END
-        
-        
+
         ";
+        DB::connection('tenant')->statement($sqlDelete);
         DB::connection('tenant')->statement($sql);
     }
 
