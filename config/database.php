27cc2196d3ad9da79f1552777695e965e292b6e9
desarrollo@joinec.net <desarrollo@joinec.net>
2024-01-09 12:42:24 -0500

    charge

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => array(
-                PDO::ATTR_EMULATE_PREPARES => true,
+                //PDO::ATTR_EMULATE_PREPARES => true,
                 PDO::MYSQL_ATTR_LOCAL_INFILE => true,
                 PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
             ),
@@ -97,7 +97,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => array_filter([
-                PDO::ATTR_EMULATE_PREPARES => true,
+                //PDO::ATTR_EMULATE_PREPARES => true,
                 PDO::MYSQL_ATTR_LOCAL_INFILE => true,
                 PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
             ]),
