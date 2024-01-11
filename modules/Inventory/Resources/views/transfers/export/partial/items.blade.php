e310b1e656994f9856322f9cde893dccf65dd50f
desarrollo@joinec.net <desarrollo@joinec.net>
2024-01-11 18:21:05 -0500

    por ajustar asinetos

--- modules/Inventory/Resources/views/transfers/export/partial/items.blade.php
+++ modules/Inventory/Resources/views/transfers/export/partial/items.blade.php
@@ -2,7 +2,7 @@
     <div class=" ">
         <table class="full-width">
             <?php
-            
+
             use App\Models\Tenant\Company;
             use App\Models\Tenant\Configuration;
             use App\Models\Tenant\User;
@@ -11,25 +11,25 @@
             use Modules\Inventory\Models\Warehouse;
             use Illuminate\Support\Carbon;
             use Illuminate\Database\Eloquent\Collection;
-            
+
             $motivo = !empty($data['motivo']) ? $data['motivo'] : null;
             /** @var Carbon $created_at */
             /** @var Warehouse $warehouse_to */
             /** @var Warehouse $warehouse_from */
             /** @var User $user */
             /** @var Collection|Inventory[] $inventories */
-            
+
             $created_at = !empty($data['created_at']) ? $data['created_at'] : Carbon::now();
             $quantity = !empty($data['quantity']) ? $data['quantity'] : 0;
-            
+
             $warehouse_from = !empty($data['warehouse_from']) ? $data['warehouse_from'] : new Warehouse();
             $warehouse_to = !empty($data['warehouse_to']) ? $data['warehouse_to'] : new Warehouse();
             $user = !empty($data['user']) ? $data['user'] : new User();
             $configuration = !empty($data['configuration']) ? $data['configuration'] : new Configuration();
             $company = !empty($data['company']) ? $data['company'] : new Company();
-            
+
             $pdf = $pdf ?? false;
-            
+
             ?>
 
 
@@ -40,7 +40,8 @@
                     <th class="fourteen-width text-left">DESCRIPCIÓN PRODUCTO</th>
                     <th class="ten-width">UNIDAD</th>
                     <th class="ten-width">CANTIDAD</th>
-                    <th class="ten-width">LOTE</th>
+                    <th class="ten-width">LOTE/SERIE</th>
+
                     <!--        <th width="10%">SERIE</th>-->
                 </tr>
             </thead>
@@ -50,16 +51,16 @@
                     /** @var \Modules\Inventory\Models\Inventory $inventory */
                     $item = $inventory->item;
                     $itemCollection = $item->getCollectionData($configuration);
-                    $itemCollection['description'] = substr($itemCollection['name'] . ' / ' . $itemCollection['description'], 0, 49);
+                    $itemCollection['description'] = substr($itemCollection['name'] . ' / ' . $itemCollection['description'].' / '.$itemCollection['model'], 0, 49);
                     $itemCollection['internal_id'] = substr($itemCollection['internal_id'], 0, 10);
                     $itemCollection['unit_type_text'] = substr($itemCollection['unit_type_text'], 0, 10);
                     $qty = $inventory->quantity;
                     $lot_code = $lote[$index]->lot_code;
-                    
+
                     /*
                 @todo BUSCAR DONDE SE GUARDA LA SERIE en modules/Inventory/Http/Controllers/TransferController.php 237
                 */
-                    
+
                     ?>
                     <tr>
                         <td class="celda text-center">{{ $index + 1 }}</td>
@@ -97,12 +98,12 @@
         <table class="full-width mt-4 mb-5" style="border: none">
             <tr>
                 <td width="50%" class="text-center">________________________________________</td>
-                <td width="50%" class="text-center">________________________________________</td>     
+                <td width="50%" class="text-center">________________________________________</td>
             </tr>
             <tr>
-                <td width="50%" class="text-center"><b>Entregado por:</b></td> 
+                <td width="50%" class="text-center"><b>Entregado por:</b></td>
                 <td width="50%" class="text-center"><b>Recibido por:</b></td>
-            </tr>  
+            </tr>
         </table>
     </div>
 </div>
