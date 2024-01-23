b38bf311dcb7f5faa0c97b2bd762a20f51928272
desarrollo@joinec.net <desarrollo@joinec.net>
2024-01-23 14:05:41 -0500

    UPDATE REVERSE UNPAID

--- resources/js/views/tenant/documents/partials/payments.vue
+++ resources/js/views/tenant/documents/partials/payments.vue
@@ -24,7 +24,7 @@
                             <tbody>
                                 <tr v-for="(row, index) in records" :key="index"   :class="{ 'text-danger border-left border-danger': (row.payment < 0), }">
                                     <template v-if="row.id">
-                                        <td>{{ (row.multi_pay && row.multipay == 'SI')?'MULTIPAGO':'PAGO'}}-{{ row.id }}</td>
+                                        <td>{{ (row.multi_pay && row.multi_pay == 'SI')?'MULTIPAGO':'PAGO'}}-{{ row.sequential }}</td>
                                         <td>{{ row.date_of_payment }}</td>
                                         <td>{{ row.payment_method_type_description }}</td>
                                         <td>{{ row.destination_description }}</td>
